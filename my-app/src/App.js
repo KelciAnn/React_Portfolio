77ca8953560634bc9dd4d905a212a3170f171949
Kelci Fowler <143914534+KelciAnn@users.noreply.github.com>
2023-10-02 14:35:31 -0700

    About Section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Introduction from './Components/Introduction/Introduction';
 import DevPics from './Components/DevPics/DevPics';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className='App'>
       <Introduction />
       <DevPics />
+      <About />
     </div>
   );
 }
